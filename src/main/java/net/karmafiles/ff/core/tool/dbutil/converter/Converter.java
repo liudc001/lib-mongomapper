2e18b039cac161b1bdf28b067936825d5d84a308
timur <timur@phoca.(none)>
2010-12-04 20:20:10 +0100

    a temporary (?) solution for FF-41

--- src/main/java/net/karmafiles/ff/core/tool/dbutil/converter/Converter.java
+++ src/main/java/net/karmafiles/ff/core/tool/dbutil/converter/Converter.java
@@ -2,6 +2,7 @@ package net.karmafiles.ff.core.tool.dbutil.converter;
 
 import com.mongodb.DBObject;
 import net.karmafiles.ff.core.tool.dbutil.converter.bean.BeanEnhancer;
+import org.apache.commons.beanutils.PropertyUtils;
 
 import java.util.Map;
 
@@ -31,7 +32,14 @@ public class Converter {
 
     @SuppressWarnings({"unchecked"})
     public static <T> T toObject(Class<T> clazz, DBObject source) {
-        return (T)BeanEnhancer.create(clazz, source);
+        T newObject = (T) BeanEnhancer.create(clazz, source);
+        try {
+            T copyObject = clazz.newInstance();
+            PropertyUtils.copyProperties(copyObject, newObject);
+            return copyObject;
+        } catch (Exception e) {
+            return newObject;
+        }
     }
 
 }
